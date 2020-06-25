67266b81250825ea7b5b8ecebc3c1ea8d728904a
Esoriak <somahaut.dev@gmail.com>
2020-06-25 23:25:49 +0200

    add searchbar and stylesheet import

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './Components/Search';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Coucou TOI ! :) </Text>
-    </View>
+      <Search />
   );
 }
 
--- Components/Search.js
+++ Components/Search.js
@@ -1,12 +1,31 @@
 import React from 'react'
 
-import {View, Button, TextInput} from 'react-native'
+import {StyleSheet, View, Button, TextInput} from 'react-native'
 
 class Search extends React.Component {
   render() {
-    return {
-
+    return (
+      <View style={styles.view}>
+        <TextInput style={styles.textinput} placeholder="Titre du film" />
+        <Button title="Rechercher" onPress={() => {}} />
+      </View>
+      )
     }
   }
-}
+
+  const styles = StyleSheet.create({
+    textinput:{
+      marginLeft : 5,
+      marginRight: 5,
+      height: 50,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft: 5,
+      marginBottom: 20
+    },
+    view :{
+      marginTop: 30
+    },
+  })
+export default Search
 
